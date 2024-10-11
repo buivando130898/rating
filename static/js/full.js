0110cc0a3bd6132cc6ca7d03cec5be4e1558652f
unknown <Admin@NURALAPTOP0.nura.vn>
2024-10-11 11:41:57 +0700

    new

--- static/js/full.js
+++ static/js/full.js
@@ -1,18 +1,19 @@
 function fullDispaly() {
-  if (document.documentElement.requestFullscreen) {
-    document.documentElement.requestFullscreen();
-  } else if (document.documentElement.mozRequestFullScreen) {
-    // Firefox
-    document.documentElement.mozRequestFullScreen();
-  } else if (document.documentElement.webkitRequestFullscreen) {
-    // Chrome, Safari and Opera
-    document.documentElement.webkitRequestFullscreen();
-  } else if (document.documentElement.msRequestFullscreen) {
-    // IE/Edge
-    document.documentElement.msRequestFullscreen();
-  }
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+        // Firefox
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+        // Chrome, Safari and Opera
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+        // IE/Edge
+        document.documentElement.msRequestFullscreen();
+    }
 
-  closeBlock("fullNone0");
-  closeBlock("fullNone1");
-  closeBlock("fullNone2");
+    closeBlock('fullNone0');
+    closeBlock('fullNone1');
+    closeBlock('fullNone2');
+    closeBlock('fullNone3');
 }
