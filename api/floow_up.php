57551ab103202af09d742a39623fdd90bcc5fb32
unknown <Admin@NURALAPTOP0.nura.vn>
2024-12-04 11:39:54 +0700

    new

--- api/floow_up.php
+++ api/floow_up.php
@@ -72,16 +72,18 @@ class api extends restful_api
                         $month =  postHandleData("month");
                         $note =  postHandleData("note");
                         $date_end = NULL;
-                        if ($month) {
-                                $date_end = addMonthsToCurrentDate($month);
-                        }
-                        // echo $date_end;
-                        $sql = "DELETE FROM rt_floow_up WHERE op_no = '$op_no' AND DATE('$time') = DATE(time_input) AND '$time' != time_input ";
 
+                        $sql = "DELETE FROM rt_floow_up WHERE op_no = '$op_no' AND DATE('$time') = DATE(time_input) AND '$time' != time_input ";
                         mysqli_set_charset($conn, 'UTF8');
                         $conn->query($sql);
 
-                        $sql = "INSERT INTO rt_floow_up(op_no, name, age, doctor_name, service_name, date_end, note, time_input) VALUE ('$op_no', '$name', $age, '$doctor_name', '$service_name', '$date_end', '$note', '$time')";
+                        if ($month) {
+                                $date_end = addMonthsToCurrentDate($month);
+                                $sql = "INSERT INTO rt_floow_up(op_no, name, age, doctor_name, service_name, date_end, note, time_input) VALUE ('$op_no', '$name', $age, '$doctor_name', '$service_name', '$date_end', '$note', '$time')";
+                        } else {
+                                $sql = "INSERT INTO rt_floow_up(op_no, name, age, doctor_name, service_name, note, time_input) VALUE ('$op_no', '$name', $age, '$doctor_name', '$service_name', '$note', '$time')";
+                        }
+
                         mysqli_set_charset($conn, 'UTF8');
                         $conn->query($sql);
                         $conn->close();
